bf47467c62c3ce9d429edb1c7b0188d3c08bde71
casdarcher <casdarcher@gmail.com>
2017-04-21 19:09:22 -0400

    Final Commit

--- js/main.js
+++ js/main.js
@@ -24,24 +24,22 @@ $( document ).ready(function() {
  
  }); 
   
-    // TODO: Create a function to listen for clicks on all the "View Details"
-    // buttons so that when a user clicks a "View Details" button they see
+    // Function that listens for clicks on all the "View Details"
+    // buttons so that when a user clicks they see
     // the content contained in the elements with the class "details" in the
     // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
-    //      2. Within that parent, find all the elements that have the class `details`.
-    //      3. Toggle visibility of all the elements within that parent with the class `details`.
-    //      4. Change the text of the "view details" button to read "hide details" so the user
-    //          understands they can hide the text again.
+ 
+ 
     $('.view-details').on('click', function(event){
         console.log(event);
         var targetElement = event.target;
         var container = targetElement.parentElement.parentElement;
+    //  Toggle visibility of all the elements within that parent with the class `details`. 
         $(container).find('.details').each(function(index, el){
           if ($(el).is(':visible')){
               $(el).fadeOut();
               targetElement.innerText = "View Details"
-
+   //   Change the text of the "view details" button to read "hide details" so the user understands they can hide the text again.
         } else {
           $(el).fadeIn();
           targetElement.innerText = "Hide Details"
